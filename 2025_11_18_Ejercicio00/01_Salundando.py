ad583d868c4aeaa4aa7399375f8766b3319610f6
yansojalle <yansojalle1@educacion.navarra.es>
2025-11-18 09:16:12 +0100

    He terminado el ejercicio00

---
+++ 2025_11_18_Ejercicio00/01_Salundando.py
@@ -0,0 +1,2 @@
+nombre = input("Introduce tu nombre:")
+print(f"Entrada: {nombre} - Salida: 'Hola {nombre}, bienvenid@'")
---
+++ 2025_11_18_Ejercicio00/03_Convertir_Min_A_Seg.py
@@ -0,0 +1,5 @@
+minutos = input("Selecciona los minutos que quieres pasar a segundos: ")
+
+minutos_num = int(minutos)
+segundos = minutos_num * 60
+print(f"Entrada: '{minutos}' minutos - Salida: '{segundos}' segundos")
